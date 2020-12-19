cadeaf2dd0c7bcc1c8c8d65ee430235d79915079
tomcowperwood <tomcowperwood@gmail.com>
2020-12-19 03:20:21 +0300

    1

--- index.php
+++ index.php
@@ -10,20 +10,62 @@
 </head>
 <body>
 <h1>Hope</h1>
-<div class="flexslider" style="width: 50vh;">
-  <ul class="slides" style="width: 50vh;">
+<?php
+    function resize_image($file, $w, $h, $crop=FALSE) {
+        list($width, $height) = getimagesize($file);
+        $r = $width / $height;
+        if ($crop) {
+            if ($width > $height) {
+                $width = ceil($width-($width*abs($r-$w/$h)));
+            } else {
+                $height = ceil($height-($height*abs($r-$w/$h)));
+            }
+            $newwidth = $w;
+            $newheight = $h;
+        } else {
+            if ($w/$h > $r) {
+                $newwidth = $h*$r;
+                $newheight = $h;
+            } else {
+                $newheight = $w/$r;
+                $newwidth = $w;
+            }
+        }
+        $src = imagecreatefromjpeg($file);
+        $dst = imagecreatetruecolor($newwidth, $newheight);
+        imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+    
+        return $dst;
+    }
+?>
+
+<!-- <div class="flexslider" >
+  <ul class="slides" >
     <li>
-      <img src="images/1.jpg" style="width: 50vh;"/>
+      <img src="images/1.jpg" />
     </li>
     <li>
-      <img src="images/2.jpg" style="width: 50vh;"/>
+      <img src="images/2.jpg" />
     </li>
     <li>
-      <img src="images/3.jpg" style="width: 50vh;"/>
+      <img src="images/3.jpg" />
+    </li>
+  </ul>
+</div> -->
+<div class="flexslider" >
+  <ul class="slides" >
+    <li>
+      <img src="<?php imagescale(imagecreatefromjpeg('images/1.jpg') , 500, 400);  ?>" />
+    </li>
+    <li>
+      <img src="<?php imagescale(imagecreatefromjpeg('images/2.jpg') , 500, 400);  ?>" />
+    </li>
+    <li>
+      <img src="<?php imagescale(imagecreatefromjpeg('images/3.jpg') , 500, 400);  ?>" />
     </li>
   </ul>
 </div>
-
+<?php resize_image('images/1.jpg', 200, 200); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
